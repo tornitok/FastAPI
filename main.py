6eea3e27be688dd40965160c877a9392a8f57681
Клим Кузнецов <klimman2000@gmail.com>
2024-08-27 17:18:27 +0200

    Continue

--- main.py
+++ main.py
@@ -1,39 +1,128 @@
-from fastapi import FastAPI
-from typing import Optional
-from enum import Enum
+# from fastapi import FastAPI, Path, Query
+# from typing import Optional
+# from enum import Enum
 
-# Создание объекта приложения.
+# # Создание объекта приложения.
+# app = FastAPI()
+
+
+# class EducationLevel(str, Enum):
+#     # Укажем значения с большой буквы, чтобы они хорошо смотрелись
+#     # в документации Swagger.
+#     SECONDARY = 'Среднее образование'
+#     SPECIAL = 'Среднее специальное образование'
+#     HIGHER = 'Высшее образовани'
+
+# # Новый эндпоинт: приветствие для автора.
+
+
+# @app.get(
+#         '/{name}',
+#         tags=['common methods', 'greetings'],
+#         summary='Общее приветствие',
+#         response_description='Полная строка приветствия'
+#     )
+# def greetings(
+#         *,
+#         # У параметров запроса name и surname значений по умолчанию нет,
+#         # поэтому в первый параметр ставим многоточие, Ellipsis.
+#         name: str = Path(
+#             ...,
+#             min_length=2,
+#             max_length=20,
+#             description='Можно вводить в любом регистре'
+#         ),
+#         surname: list[str] = Query(..., min_length=2, max_length=50),
+#         cyrillic_string: str = Query(
+#             'Здесь только кириллица',
+#             regex='^[А-Яа-яЁё ]+$'
+#         ),
+#         age: Optional[int] = Query(None, gt=4, le=99),
+#         is_staff: bool = Query(
+#             False,
+#             alias='is-staff',
+#             include_in_schema=False
+#         ),
+#         education_level: Optional[EducationLevel] = None,
+# ) -> dict[str, str]:
+#     """
+#     Приветствие пользователя:
+
+#     - **name**: имя
+#     - **surname**: фамилия
+#     - **age**: возраст (опционально)
+#     - **is_staff**: является ли пользователь сотрудником
+#     - **education_level**: уровень образования (опционально)
+#     """
+#     surnames = ' '.join(surname)
+#     result = ' '.join([name, surnames])
+#     result = result.title()
+#     if age is not None:
+#         result += ', ' + str(age)
+#     if education_level is not None:
+#         result += ', ' + education_level.lower()
+#     if is_staff:
+#         result += ', сотрудник'
+#     return {'Hello': result}
+
+
+# @app.get(
+#         '/me',
+#         tags=['special methods', 'greetings'],
+#         summary='Приветствие автора',
+#         description='Приветствие человека по имени и фамилии; '
+#                 'опционально указывается возраст, '
+#                 'образование и статус сотрудника',
+#     )
+# def hello_author():
+#     return {'Hello': 'author'}
+
+from enum import Enum
+from typing import Optional
+
+from fastapi import FastAPI
+# Для работы с JSON в теле запроса
+# импортируем из pydantic класс BaseModel
+from pydantic import BaseModel
+
 app = FastAPI()
 
 
 class EducationLevel(str, Enum):
-    # Укажем значения с большой буквы, чтобы они хорошо смотрелись
-    # в документации Swagger.
     SECONDARY = 'Среднее образование'
     SPECIAL = 'Среднее специальное образование'
-    HIGHER = 'Высшее образовани'
-
-# Новый эндпоинт: приветствие для автора.
+    HIGHER = 'Высшее образование'
 
 
-@app.get('/{name}')
-def greetings(
-        *,
-        name: str,
-        surname: str,
-        age: Optional[int] = None,
-        is_staff: bool = False,
-        education_level: Optional[EducationLevel] = None,
-) -> dict[str, str]:
-    result = ' '.join([name, surname, education_level])
+# Создадим класс Person, унаследованный от BaseModel;
+# в атрибутах класса перечислим ожидаемые параметры запроса.
+# Аннотируем атрибуты класса.
+class Person(BaseModel):
+    name: str
+    surname: list[str]
+    age: Optional[int]
+    is_staff: bool = False
+    education_level: Optional[EducationLevel]
+
+
+# Меняем метод GET на POST, указываем статичный адрес.
+@app.post('/hello')
+# Вместо множества параметра теперь будет только один - person,
+# в качестве аннотации указываем класс Person.
+def greetings(person: Person) -> dict[str, str]:
+    # Обращение к атрибутам класса происходит через точку;
+    # при этом будут работать проверки на уровне типов данных.
+    # В IDE будут работать автодополнения.
+    if isinstance(person.surname, list):
+        surnames = ' '.join(person.surname)
+    else:
+        surnames = person.surname
+    result = ' '.join([person.name, surnames])
     result = result.title()
-    if age is not None:
-        result += ', ' + str(age)
-    if is_staff:
+    if person.age is not None:
+        result += ', ' + str(person.age)
+    if person.education_level is not None:
+        result += ', ' + person.education_level.lower()
+    if person.is_staff:
         result += ', сотрудник'
     return {'Hello': result}
-
-
-@app.get('/me')
-def hello_author():
-    return {'Hello': 'author'}
